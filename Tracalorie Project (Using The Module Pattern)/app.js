d58cd443e66858d2326982763093833fc9823157
abinthomas15 <abinthomas156@gmail.com>
2021-04-16 16:25:02 +0530

    completed data structures and controllers of tracalorie project

---
+++ Tracalorie Project (Using The Module Pattern)/app.js
@@ -0,0 +1,38 @@
+// Storage Controller
+
+// Item Controller
+  const ItemCtrl = (function() {
+    // Item Constructor
+    const Item = function(id, name, calories){
+      this.id = id;
+      this.name = name;
+      this.calories = calories;
+    }
+    // Data Structure / State
+    const data = {
+      items: [
+        {id: 0, name: 'Steak Dinner', calories: 1200},
+        {id: 0, name: 'Cookie', calories: 400},
+        {id: 0, name: 'Eggs', calories: 300}
+      ],
+      currentItem : null,
+      totalCalories : 0
+    }
+    return {
+      logData : function () {
+        return data;
+      }
+    }
+  }) ();
+
+// UI Controller
+const UICtrl = (function() {
+  
+}) ();
+
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl) {
+  
+  console.log(ItemCtrl.logData());
+}) (ItemCtrl, UICtrl);
