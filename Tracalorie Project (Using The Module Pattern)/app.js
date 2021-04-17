3a897c638317810c76590010a2914222e9a10722
abinthomas15 <abinthomas156@gmail.com>
2021-04-17 23:37:36 +0530

    added items to the UI

--- Tracalorie Project (Using The Module Pattern)/app.js
+++ Tracalorie Project (Using The Module Pattern)/app.js
@@ -43,6 +43,16 @@
 
         return newItem;
       },
+      getItemById : function(id) {
+        let found = null ;
+        // Loop throug the items
+        data.items.forEach(function(item){
+          if(item.id === id) {
+            found = item;
+          }
+        });
+        return found;
+      },
       getTotalCalories : function () {
         let total = 0;
         // Loop through items and add cals
@@ -68,6 +78,9 @@ const UICtrl = (function() {
   const UISelectors = {
     itemList : '#item-list',
     addBtn :'.add-btn',
+    updateBtn : '.update-btn',
+    deleteBtn : '.delete-btn',
+    backBtn : '.back-btn',
     itemNameInput : '#item-name',
     itemCaloriesInput : '#item-calories',
     totalCalories: '.total-calories'
@@ -126,6 +139,14 @@ const UICtrl = (function() {
       document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
     },
 
+    clearEditState : function () {
+      UICtrl.clearInput();
+      document.querySelector(UISelectors.updateBtn).style.display = 'none';
+      document.querySelector(UISelectors.deleteBtn).style.display = 'none';
+      document.querySelector(UISelectors.backBtn).style.display = 'none';
+      document.querySelector(UISelectors.addBtn).style.display = 'inlines';
+    },
+
     getSelectors : function () {
       return UISelectors;
     }
@@ -143,6 +164,9 @@ const App = (function(ItemCtrl, UICtrl) {
 
     // Add item event
     document.querySelector(UISelectors.addBtn).addEventListener('click',itemAddSubmit);
+
+    // Edit icon click event
+    document.querySelector(UISelectors.itemList).addEventListener('click',itemUpdateSubmit)
   }
   
   // Add item submit
@@ -164,9 +188,35 @@ const App = (function(ItemCtrl, UICtrl) {
 
       e.preventDefault();
     }
+    
+    // Update item submit
+    const itemUpdateSubmit = function (e) {
+      if(e.target.classList.contains('edit-item')) {
+
+        // Get item list id (item-0, item-1)
+        const listId = e.target.parentNode.parentNode.id;
+
+        // Break into array
+        const listIdArr = listId.split('-');
+
+        // Get the actual id
+        const id = parseInt(listIdArr[1]);
+
+        // Get item
+        const itemToEdit = itemCtrl.getItemById(id);
+
+        console.log(itemToEdit);
+      }
+
+      e.preventDefault();
+    }
+
   // Public methods
   return {
     init: function () {
+
+      // Clear edit state 
+      UICtrl.clearEditState();
       // Fetch Item from data structres
       const items = ItemCtrl.getItems();
       
